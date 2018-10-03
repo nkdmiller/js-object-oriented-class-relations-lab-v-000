8d6a2cced3ddf8fbd147d532bcfa5cdb559f6d90
Nate Miller <natekmiller96@gmail.com>
2018-10-03 23:23:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,14 +71,14 @@ class Trip {
         store.trips.push(this);
     }
     driver() {
-        return store.drivers.filter(
+        return store.drivers.find(
             function(driver) {
                 return driver.id === this.driverId;
             }.bind(this)
         );
     }
     passenger() {
-        return store.passengers.filter(
+        return store.passengers.find(
             function(passenger) {
                 return passenger.id === this.passengerId;
             }.bind(this)
