6a17adc26e69029a03d8ca43ef740c86c8a7f254
Nate Miller <natekmiller96@gmail.com>
2018-10-03 23:58:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,93 @@
+let store = { drivers: [], trips: [], passengers: [] };
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+class Driver {
+    constructor(name) {
+        this.id = ++driverId;
+        this.name = name;
+        // if (user) {
+        //     this.userId = user.id;
+        // }
+ 
+        // insert in the item to the store
+        store.drivers.push(this);
+    }
+    trips() {
+        return store.trips.filter(
+            function(trip) {
+                return trip.driverId === this.id;
+            }.bind(this)
+        );
+    }
+    passengers() {
+        return store.trips.filter(
+            function(trip) {
+                if (trip.driverId === this.id){
+                  return trip.passenger();
+                }
+            }.bind(this)
+        );      
+    }
+}
+
+class Passenger {
+    constructor(name) {
+        this.id = ++passengerId;
+        this.name = name;
+        // if (user) {
+        //     this.userId = user.id;
+        // }
+ 
+        // insert in the item to the store
+        store.passengers.push(this);
+    }
+    trips() {
+        return store.trips.filter(
+            function(trip) {
+                return trip.passengerId === this.id;
+            }.bind(this)
+        );
+    }
+    drivers() {
+        return store.trips.filter(
+            function(trip) {
+                if (trip.passengerId === this.id){
+                  return store.drivers.find{
+                    return driver.id === trip.driverId;
+                  }
+                }
+            }.bind(this)
+        );      
+    }
+}
+
+class Trip {
+    constructor(driver, passenger) {
+        this.id = ++tripId;
+        if (driver) {
+            this.driverId = driver.id;
+         }
+         
+         if (passenger) {
+           this.passengerId = passenger.id;
+         }
+ 
+        // insert in the item to the store
+        store.trips.push(this);
+    }
+    driver() {
+        return store.drivers.find(
+            function(driver) {
+                return driver.id === this.driverId;
+            }.bind(this)
+        );
+    }
+    passenger() {
+        return store.passengers.find(
+            function(passenger) {
+                return passenger.id === this.passengerId;
+            }.bind(this)
+        );
+    }
+}
